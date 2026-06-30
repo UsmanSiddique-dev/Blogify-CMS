1ec4fded7cd92527fe2335b1771a2d1d77388ca7
Prime Labs <mrcody497@gmail.com>
2026-06-30 13:07:25 +0500

    feat(admin): enhance category management with error handling and update functionality

--- controllers/categorycontroller.js
+++ controllers/categorycontroller.js
@@ -38,10 +38,53 @@ export const addCategory = async (req, res) => {
   }
 };
 export const updateCategoryPage = async (req, res) => {
-  res.render("admin/categories/update", {
-    layout: "admin/layout",
-    role: req.role,
-  });
+  try {
+    const category = await CategoryModel.findById(req.params.id);
+
+    if (!category) {
+      return res.status(404).send("Category not found");
+    }
+
+    res.render("admin/categories/update", {
+      layout: "admin/layout",
+      role: req.role,
+      category: category,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Internal server error");
+  }
 };
-export const updateCategory = async (req, res) => {};
-export const deleteCategory = async (req, res) => {};
+export const updateCategory = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const category = await CategoryModel.findByIdAndUpdate(id, {
+      title: req.body.title,
+      description: req.body.description,
+    });
+
+    if (!category) {
+      return res.status(404).send("Category not found");
+    }
+
+    return res.redirect("/admin/category");
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Internal server error");
+  }
+};
+export const deleteCategory = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const category = await CategoryModel.findByIdAndDelete(id);
+
+    if (!category) {
+      return res.status(404).send("Category not found");
+    }
+
+    return res.redirect("/admin/category");
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Internal server error");
+  }
+};
